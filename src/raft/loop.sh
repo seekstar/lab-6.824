1792d83eee754487ae7289ee9588815e16cb86a7
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2022-05-09 20:18:25 +0800

    Optional arg for loop.sh

--- src/raft/loop.sh
+++ src/raft/loop.sh
@@ -1,7 +1,14 @@
 i=1
 # The exit code of "go test -run $1 | tee log.txt" is incorrect!
-while go test -run $1 > log.txt; do
-    echo $i
-    i=$((i+1))
-done
+if [ $1 ]; then
+	while go test -run $1 > log.txt; do
+		echo $i
+		i=$((i+1))
+	done
+else
+	while go test > log.txt; do
+		echo $i
+		i=$((i+1))
+	done
+fi
 echo $i failed
